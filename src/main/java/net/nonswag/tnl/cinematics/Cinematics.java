3fdec1912c3734540b9aeb6e149078b7e01a8119
NonSwag <mrminecraft00@gmail.com>
2022-11-16 21:44:04 +0100

    removed redundant code

--- src/main/java/net/nonswag/tnl/cinematics/Cinematics.java
+++ src/main/java/net/nonswag/tnl/cinematics/Cinematics.java
@@ -6,27 +6,13 @@ import net.nonswag.tnl.listener.api.plugin.PluginUpdate;
 import net.nonswag.tnl.listener.api.plugin.TNLPlugin;
 import net.nonswag.tnl.listener.api.settings.Settings;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 public class Cinematics extends TNLPlugin {
-
-    @Nullable
-    private static Cinematics instance = null;
-
     @Override
     public void enable() {
-        instance = this;
         Messages.init();
         getCommandManager().registerCommand(new CinematicCommand());
         async(() -> {
             if (Settings.AUTO_UPDATER.getValue()) new PluginUpdate(this).downloadUpdate();
         });
-    }
-
-    @Nonnull
-    public static Cinematics getInstance() {
-        assert instance != null;
-        return instance;
     }
 }
