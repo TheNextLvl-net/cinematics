961c11f373a389f3db4053c9c371ac7c8a49abda
NonSwag <mrminecraft00@gmail.com>
2022-12-31 13:35:49 +0100

    1.19.3 update

--- src/main/java/net/nonswag/tnl/cinematics/Cinematics.java
+++ src/main/java/net/nonswag/tnl/cinematics/Cinematics.java
@@ -2,17 +2,16 @@ package net.nonswag.tnl.cinematics;
 
 import net.nonswag.tnl.cinematics.commands.CinematicCommand;
 import net.nonswag.tnl.cinematics.utils.Messages;
-import net.nonswag.tnl.listener.api.plugin.PluginUpdate;
 import net.nonswag.tnl.listener.api.plugin.TNLPlugin;
-import net.nonswag.tnl.listener.api.settings.Settings;
 
 public class Cinematics extends TNLPlugin {
     @Override
-    public void enable() {
+    public void load() {
         Messages.init();
+    }
+
+    @Override
+    public void enable() {
         getCommandManager().registerCommand(new CinematicCommand());
-        async(() -> {
-            if (Settings.AUTO_UPDATER.getValue()) new PluginUpdate(this).downloadUpdate();
-        });
     }
 }
--- src/main/java/net/nonswag/tnl/cinematics/utils/Messages.java
+++ src/main/java/net/nonswag/tnl/cinematics/utils/Messages.java
@@ -1,7 +1,8 @@
 package net.nonswag.tnl.cinematics.utils;
 
 import net.nonswag.core.api.annotation.FieldsAreNonnullByDefault;
-import net.nonswag.core.api.message.Message;
+import net.nonswag.core.api.file.formats.MessageFile;
+import net.nonswag.core.api.language.Language;
 import net.nonswag.core.api.message.key.MessageKey;
 
 @FieldsAreNonnullByDefault
@@ -17,18 +18,20 @@ public final class Messages {
     }
 
     private static void initEnglish() {
-        Message.getEnglish().setMessage(RECORD_NOT_FOUND, "%prefix% §cA record with this name does not exist");
-        Message.getEnglish().setMessage(RECORDING_ERROR, "%prefix% §cAn error has occurred while recording");
-        Message.getEnglish().setMessage(RECORDING_SAVED, "%prefix% §aSaved recording §6%recording%");
-        Message.getEnglish().setMessage(RECORDING_STOPPED, "%prefix% §aStopped the recording");
-        Message.getEnglish().save();
+        MessageFile english = MessageFile.getOrCreate(Language.AMERICAN_ENGLISH);
+        english.setMessage(RECORD_NOT_FOUND, "%prefix% §cA record with this name does not exist");
+        english.setMessage(RECORDING_ERROR, "%prefix% §cAn error has occurred while recording");
+        english.setMessage(RECORDING_SAVED, "%prefix% §aSaved recording §6%recording%");
+        english.setMessage(RECORDING_STOPPED, "%prefix% §aStopped the recording");
+        english.save();
     }
 
     private static void initGerman() {
-        Message.getGerman().setMessage(RECORD_NOT_FOUND, "%prefix% §cEs existiert keine Aufnahme mit diesem Namen");
-        Message.getGerman().setMessage(RECORDING_ERROR, "%prefix% §cWährend der Aufnahme ist ein Fehler aufgetreten");
-        Message.getGerman().setMessage(RECORDING_SAVED, "%prefix% §aDie Aufnahme §6%recording%§a wurde gespeichert");
-        Message.getGerman().setMessage(RECORDING_STOPPED, "%prefix% §aDie Aufnahme wurde gestoppt");
-        Message.getGerman().save();
+        MessageFile german = MessageFile.getOrCreate(Language.GERMAN);
+        german.setMessage(RECORD_NOT_FOUND, "%prefix% §cEs existiert keine Aufnahme mit diesem Namen");
+        german.setMessage(RECORDING_ERROR, "%prefix% §cWährend der Aufnahme ist ein Fehler aufgetreten");
+        german.setMessage(RECORDING_SAVED, "%prefix% §aDie Aufnahme §6%recording%§a wurde gespeichert");
+        german.setMessage(RECORDING_STOPPED, "%prefix% §aDie Aufnahme wurde gestoppt");
+        german.save();
     }
 }
