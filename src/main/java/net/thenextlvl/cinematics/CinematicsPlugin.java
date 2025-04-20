ce2a9d557d027b6ec5ec084eda7a6ddf24b9e675
david <github@nonswag.dev>
2025-04-20 09:48:31 +0200

    Refactored package structure for commands

--- src/main/java/net/thenextlvl/cinematics/CinematicsPlugin.java
+++ src/main/java/net/thenextlvl/cinematics/CinematicsPlugin.java
@@ -1,7 +1,7 @@
 package net.thenextlvl.cinematics;
 
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-import net.thenextlvl.cinematics.commands.CinematicCommand;
+import net.thenextlvl.cinematics.command.CinematicCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CinematicsPlugin extends JavaPlugin {
